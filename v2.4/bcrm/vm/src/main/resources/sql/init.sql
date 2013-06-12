462c4bd6fc33ebb6ee85017f97a7ad459c3404d8
golthiryus@gmail.com <golthiryus@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-06-12 15:35:49 +0000

    BCRM with AG v2.4

---
+++ v2.4/bcrm/vm/src/main/resources/sql/init.sql
@@ -0,0 +1,10 @@
+/*
+enum Role {
+    MANAGER // 0
+    SALESPERSON // 1
+}
+*/
+insert into Employee (login,password,myRole) values ('alice', '1111',0);
+insert into Employee (login,password,myRole) values ('bob', '1111',1);
+insert into Employee (login,password,myRole) values ('charlie', '1111',1);
+
