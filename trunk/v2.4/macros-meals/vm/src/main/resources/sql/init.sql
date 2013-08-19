40e7207dd3e4a91fd7a99ac2cd3fc452f66b1c69
manuelgclavel@gmail.com <manuelgclavel@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-08-19 01:26:41 +0000

    Working on the meals-by-macros example. Very productive.

--- trunk/v2.4/macros-meals/vm/src/main/resources/sql/init.sql
+++ trunk/v2.4/macros-meals/vm/src/main/resources/sql/init.sql
@@ -1,5 +1,5 @@
-insert into mmeals.User (pk, login, password, myRole) values (1, 'mgc', '111', 0);
-insert into mmeals.User (pk, login, password, myRole) values (2, 'scl', '111', 1);
+insert into mmeals.User (pk, name, login, password, myRole) values (1, 'Manuel', 'Clavel', 'mgc', '111', 0);
+insert into mmeals.User (pk, surname, login, password, myRole) values (2, '', '' , 'scl', '111', 1);
 
 insert into mmeals.Meal (pk,id,literal) values (1,1,'Breakfast'), (2,2,'Lunch'), (3,3,'Dinner');
 
