ca02c453fa691cdaa503098ae2a03a514d399187
manuelgclavel@gmail.com <manuelgclavel@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-08-22 00:29:55 +0000

    Working on the project Employee.

---
+++ trunk/v2.3/Employee/vm/src/main/resources/sql/init.sql
@@ -0,0 +1,2 @@
+insert into Employee.Employee (pk, id, login, password, myRole) values (1, '101', 'bob', '111', 1);
+insert into Employee.Employee (pk, id, login, password, myRole) values (2, '201', 'alice', '111', 2);
