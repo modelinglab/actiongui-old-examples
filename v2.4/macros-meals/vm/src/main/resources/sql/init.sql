b30d58e9f4c77bf1feacc101b88b861426a64ef1
manuelgclavel@gmail.com <manuelgclavel@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-08-26 10:29:48 +0000

    updating

--- v2.4/macros-meals/vm/src/main/resources/sql/init.sql
+++ v2.4/macros-meals/vm/src/main/resources/sql/init.sql
@@ -3,5 +3,5 @@ insert into mmeals.User (pk, name, surname, login, password, myRole) values (1, 
 insert into mmeals.User (pk, name, surname, login, password, myRole) values (2, '', '' , 'scl', '111', 1);
 
 
-insert into mmeals.Meal (pk,id,literal) values (1,1,'Breakfast'), (2,2,'Lunch'), (3,3,'Dinner');
+insert into mmeals.Meal (pk,literal) values (1,'Breakfast'), (2,'Lunch'), (3, 'Dinner');
 
