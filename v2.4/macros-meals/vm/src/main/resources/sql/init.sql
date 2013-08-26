8f708f74d1d57a606078f3ab173ec18d4d97fb93
manuelgclavel@gmail.com <manuelgclavel@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-08-26 09:39:47 +0000

    Desperate. A little bit desperate.

--- v2.4/macros-meals/vm/src/main/resources/sql/init.sql
+++ v2.4/macros-meals/vm/src/main/resources/sql/init.sql
@@ -1,12 +1,7 @@
+
 insert into mmeals.User (pk, name, surname, login, password, myRole) values (1, 'Manuel', 'Clavel', 'mgc', '111', 0);
 insert into mmeals.User (pk, name, surname, login, password, myRole) values (2, '', '' , 'scl', '111', 1);
 
+
 insert into mmeals.Meal (pk,id,literal) values (1,1,'Breakfast'), (2,2,'Lunch'), (3,3,'Dinner');
 
-insert into mmeals.MealOption (pk,id,meal,literal) values (1,1,1,'Nothing'), (2,2,1,'Early Breakfast'), (3,3,1,'Normal Breakfast'), (4,4,1,'Normal Breakfast'), (5,5,1,'Take Away Early Breakfast'), (6,6,1,'Take Away Early Breakfast');
-insert into mmeals.MealOption (pk,id,meal,literal) values (7,7,2,'Nothing'), (8,8,2,'Normal Lunch'), (9,9,2,'Take Away Lunch');
-insert into mmeals.MealOption (pk,id,meal,literal) values (10,10,3,'Nothing'), (11,11,3,'Normal Dinner'), (12,12,3,'Late Dinner');
-insert into mmeals.MealOption_ownedBy__Meal_options (MealOption_ownedBy,Meal_options) values (1,1), (2,1), (3,1), (4,1), (5,1), (6,1), (7,2), (8,2), (9,2), (10,3), (11,3), (12,3);
-
-insert into mmeals.MealOptionDeadline (pk,id,literal,type,cday,chour,cminute) values (1,1,'l1',0,0,8,1), (2,2,'l2',1,0,8,2), (3,3,'l3',2,0,8,3), (4,4,'l4',3,0,8,4), (5,5,'l5',4,0,8,5), (6,6,'l6',5,0,8,6), (7,7,'l7',6,0,8,7), (8,8,'l8',7,0,8,8), (9,9,'l9',0,0,8,9), (10,10,'l10',1,0,8,10), (11,11,'l11',2,0,8,11), (12,12,'l12',3,0,8,12);
-insert into mmeals.MealOptionDeadline_mealOption__MealOption_deadlines (MealOptionDeadline_mealOption,MealOption_deadlines) values (1,1), (2,2), (3,3), (4,4), (5,5), (6,6), (7,7), (8,8), (9,9), (10,10), (11,11), (12,12);
