e9f9569ba082357cde9bace9d56d27d51ac5d01c
manuelgclavel@gmail.com <manuelgclavel@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-08-29 10:34:14 +0000

    Some problems. Need help.

--- v2.4/macros-meals/vm/src/main/resources/sql/init.sql
+++ v2.4/macros-meals/vm/src/main/resources/sql/init.sql
@@ -1,6 +1,6 @@
 
-insert into mmeals.User (pk, name, surname, login, password, myRole) values (1, 'Manuel', 'Clavel', 'mgc', '111', 0);
-insert into mmeals.User (pk, name, surname, login, password, myRole) values (2, '', '' , 'scl', '111', 1);
+insert into mmeals.User (pk, name, surname, login, password, role) values (1, 'Manuel', 'Clavel', 'mgc', '111', 0);
+insert into mmeals.User (pk, name, surname, login, password, role) values (2, '', '' , 'scl', '111', 1);
 
 
 insert into mmeals.Meal (pk,literal) values (1,'Breakfast'), (2,'Lunch'), (3, 'Dinner');
