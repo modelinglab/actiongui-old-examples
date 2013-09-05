4bc0b4dfaed788567e20cbfe66a4336868f4faf6
golthiryus@gmail.com <golthiryus@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-09-05 13:04:18 +0000

    Created a new Meals project without macros.

---
+++ v2.4/meals/vm/src/main/resources/sql/init.sql
@@ -0,0 +1,7 @@
+
+insert into mmeals.User (pk, name, surname, login, password, role) values (1, 'Manuel', 'Clavel', 'mgc', '111', 0);
+insert into mmeals.User (pk, name, surname, login, password, role) values (2, '', '' , 'scl', '111', 1);
+
+
+insert into mmeals.Meal (pk,literal) values (1,'Breakfast'), (2,'Lunch'), (3, 'Dinner');
+
