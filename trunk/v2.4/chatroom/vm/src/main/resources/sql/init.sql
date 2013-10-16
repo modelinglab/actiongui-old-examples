43cc1033ed8112995995a8385947886c42db0532
miguelangelgarciadedios@gmail.com <miguelangelgarciadedios@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-10-16 10:27:02 +0000

    Chatroom application in version 2.4

---
+++ trunk/v2.4/chatroom/vm/src/main/resources/sql/init.sql
@@ -0,0 +1,11 @@
+/*
+enum Role {
+    ANONYMOUS
+    REGISTERED
+    ADMIN
+}
+*/
+
+insert into User (nickname, login, password, role) values ("Alice", "ali", "ali", 2);
+insert into User (nickname, login, password, role) values ("Bob", "bob", "bob", 1);
+insert into User (nickname, login, password, role) values ("Charlie", "cha", "cha", 1);
