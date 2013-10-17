915f29b548e1124697c8a50d7211210ef6f9fafd
miguelangelgarciadedios@gmail.com <miguelangelgarciadedios@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-10-17 07:41:33 +0000

    - Added a new role SYSTEM that can reset the application data to the initial scenario. - Added titles to windows. - Fixed a minor bug in ANONYMOUS view of the chat rooms.

--- trunk/v2.4/chatroom/vm/src/main/resources/sql/init.sql
+++ trunk/v2.4/chatroom/vm/src/main/resources/sql/init.sql
@@ -3,9 +3,11 @@ enum Role {
     ANONYMOUS
     REGISTERED
     ADMIN
+    SYSTEM
 }
 */
 
 insert into User (nickname, login, password, role) values ("Alice", "ali", "ali", 2);
 insert into User (nickname, login, password, role) values ("Bob", "bob", "bob", 1);
 insert into User (nickname, login, password, role) values ("Charlie", "cha", "cha", 1);
+insert into User (nickname, login, password, role) values ("System", "sys", "sys", 3);
