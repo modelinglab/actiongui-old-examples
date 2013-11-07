58a94c4051e82b281743f758e0cb0a775706e3cb
manuelgclavel@gmail.com <manuelgclavel@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-11-07 07:06:24 +0000

    adding guests feature

--- v2.4/macros-meals/vm/src/main/resources/sql/init.sql
+++ v2.4/macros-meals/vm/src/main/resources/sql/init.sql
@@ -7,15 +7,14 @@ enum Role {
 }
 */
 
-insert into mmeals.Residence (pk,name) values (1,'Allenmoos');
+INSERT INTO Residence (pk, name, zone) VALUES (1,'Allenmoos','Europe/Zurich');
 
-insert into mmeals.User (pk, name, surname, login, password, role) values (1, 'Manuel', 'Clavel', 'mgc', '111', 0);
-insert into mmeals.User (pk, name, surname, login, password, role) values (2, '', '' , 'scl', '111', 1);
-insert into mmeals.User (pk, name, surname, login, password, role) values (3, '', '' , 'adm', '111', 2);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (1, 'Manuel', 'Clavel', 'mgc', '111', 0, 1);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (2, '', '' , 'scl', '111', 1, 1);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (3, '', '' , 'adm', '111', 2, 1);
 insert into Residence_users__User_residence(Residence_users,User_residence) values (1,1),(1,2),(1,3);
 
 
-insert into mmeals.Meal (pk,literal) values (1,'Breakfast'), (2,'Lunch'), (3, 'Dinner');
+insert into mmeals.Meal (pk,literal,position, residence) values (1,'Breakfast',1, 1), (2,'Lunch',2, 1), (3, 'Dinner',3, 1);
 insert into Meal_residence__Residence_meals (Meal_residence,Residence_meals) values (1,1),(2,1),(3,1);
-
 
