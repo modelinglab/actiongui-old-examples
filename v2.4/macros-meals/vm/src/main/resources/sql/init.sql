2be54d05e77649b2cbd5711a26f44f9b316c0544
manuelgclavel@gmail.com <manuelgclavel@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-11-12 05:26:36 +0000

    typo

--- v2.4/macros-meals/vm/src/main/resources/sql/init.sql
+++ v2.4/macros-meals/vm/src/main/resources/sql/init.sql
@@ -20,7 +20,7 @@ insert into mmeals.User (pk, name, surname, login, password, role, residence) va
 insert into mmeals.User (pk, name, surname, login, password, role, residence) values (4, '', '' , 'adm2', '111', 2, 2);
 insert into Residence_users__User_residence(Residence_users,User_residence) values (2,3),(2,4);
 
-INSERT INTO Residence (pk, name, zone,lang) VALUES (1,'Utopia','Europe/Madrid', 'es');
+INSERT INTO Residence (pk, name, zone,lang) VALUES (3,'Utopia','Europe/Madrid', 'es');
 insert into mmeals.User (pk, name, surname, login, password, role, residence) values (5, '', '' , 'scl', '111', 1, 3);
 insert into mmeals.User (pk, name, surname, login, password, role, residence) values (6, '', '' , 'adm', '111', 2, 3);
 insert into Residence_users__User_residence(Residence_users,User_residence) values (3,5),(3,6);
