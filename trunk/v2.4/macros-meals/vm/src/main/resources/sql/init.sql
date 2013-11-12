272e426b18c0e74b1ae171713493a3a66a3e8b14
manuelgclavel@gmail.com <manuelgclavel@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-11-12 14:15:49 +0000

    init

--- trunk/v2.4/macros-meals/vm/src/main/resources/sql/init.sql
+++ trunk/v2.4/macros-meals/vm/src/main/resources/sql/init.sql
@@ -10,19 +10,19 @@ enum Role {
 
 
 INSERT INTO Residence (pk, name, zone,lang) VALUES (1,'Allenmoos','Europe/Zurich', 'en');
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (1, '', '' , 'scl1', '111', 1, 1);
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (2, '', '' , 'adm1', '111', 2, 1);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (1, '', '' , 'scl', '111', 1, 1);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (2, '', '' , 'adm', '111', 2, 1);
 insert into Residence_users__User_residence(Residence_users,User_residence) values (1,1),(1,2);
 
 
 INSERT INTO Residence (pk, name, zone,lang) VALUES (2,'Schweidt','CET', 'de');
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (3, '', '' , 'scl2', '111', 1, 2);
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (4, '', '' , 'adm2', '111', 2, 2);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (3, '', '' , 'scl1', '111', 1, 2);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (4, '', '' , 'adm1', '111', 2, 2);
 insert into Residence_users__User_residence(Residence_users,User_residence) values (2,3),(2,4);
 
 INSERT INTO Residence (pk, name, zone,lang) VALUES (3,'Utopia','Europe/Madrid', 'es');
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (5, '', '' , 'scl', '111', 1, 3);
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (6, '', '' , 'adm', '111', 2, 3);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (5, '', '' , 'sclu', '111', 1, 3);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (6, '', '' , 'admu', '111', 2, 3);
 insert into Residence_users__User_residence(Residence_users,User_residence) values (3,5),(3,6);
 
 
