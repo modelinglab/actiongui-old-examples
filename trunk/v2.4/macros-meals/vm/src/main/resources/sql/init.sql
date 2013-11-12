649b40ee36224751fdcb253013a5c36ea3e943c5
manuelgclavel@gmail.com <manuelgclavel@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-11-12 05:17:58 +0000

    adding lang

--- trunk/v2.4/macros-meals/vm/src/main/resources/sql/init.sql
+++ trunk/v2.4/macros-meals/vm/src/main/resources/sql/init.sql
@@ -7,28 +7,23 @@ enum Role {
 }
 */
 
-INSERT INTO Residence (pk, name, zone) VALUES (1,'Utopia','Europe/Madrid');
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (1, 'Manuel', 'Clavel', 'mgc', '111', 0, 1);
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (2, '', '' , 'scl', '111', 1, 1);
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (3, '', '' , 'adm', '111', 2, 1);
-insert into Residence_users__User_residence(Residence_users,User_residence) values (1,1),(1,2),(1,3);
 
 
-insert into mmeals.Meal (pk,literal,position, residence) values (1,'Breakfast',1, 1), (2,'Lunch',2, 1), (3, 'Dinner',3, 1);
-insert into Meal_residence__Residence_meals (Meal_residence,Residence_meals) values (1,1),(2,1),(3,1);
+INSERT INTO Residence (pk, name, zone,lang) VALUES (1,'Allenmoos','Europe/Zurich', 'en');
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (1, '', '' , 'scl1', '111', 1, 1);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (2, '', '' , 'adm1', '111', 2, 1);
+insert into Residence_users__User_residence(Residence_users,User_residence) values (1,1),(1,2);
 
 
-INSERT INTO Residence (pk, name, zone) VALUES (2,'Allenmoos','Europe/Zurich');
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (4, '', '' , 'scl1', '111', 1, 2);
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (5, '', '' , 'adm1', '111', 2, 2);
-insert into Residence_users__User_residence(Residence_users,User_residence) values (2,4),(2,5);
+INSERT INTO Residence (pk, name, zone,lang) VALUES (2,'Schweidt','CET', 'de');
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (3, '', '' , 'scl2', '111', 1, 2);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (4, '', '' , 'adm2', '111', 2, 2);
+insert into Residence_users__User_residence(Residence_users,User_residence) values (2,3),(2,4);
 
-
-INSERT INTO Residence (pk, name, zone) VALUES (3,'Schweidt','CET');
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (6, '', '' , 'scl2', '111', 1, 3);
-insert into mmeals.User (pk, name, surname, login, password, role, residence) values (7, '', '' , 'adm2', '111', 2, 3);
-insert into Residence_users__User_residence(Residence_users,User_residence) values (3,6),(3,7);
-
+INSERT INTO Residence (pk, name, zone,lang) VALUES (1,'Utopia','Europe/Madrid', 'es');
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (5, '', '' , 'scl', '111', 1, 3);
+insert into mmeals.User (pk, name, surname, login, password, role, residence) values (6, '', '' , 'adm', '111', 2, 3);
+insert into Residence_users__User_residence(Residence_users,User_residence) values (3,5),(3,6);
 
 
 
