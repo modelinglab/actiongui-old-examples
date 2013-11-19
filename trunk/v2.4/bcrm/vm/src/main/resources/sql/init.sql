9c2b775af63a2e2689a276170c099b5fbfb1fdf3
golthiryus@gmail.com <golthiryus@gmail.com@aa9d44f2-b2ff-85c8-8ce0-937f9caa1f24>
2013-11-19 17:39:52 +0000

    Modified BCRM v2.4 	+ Now it works with encrypted passwords. 	+ Some labels are internationalized

--- trunk/v2.4/bcrm/vm/src/main/resources/sql/init.sql
+++ trunk/v2.4/bcrm/vm/src/main/resources/sql/init.sql
@@ -1,10 +1,5 @@
-/*
-enum Role {
-    MANAGER // 0
-    SALESPERSON // 1
-}
-*/
-insert into Employee (login,password,myRole) values ('alice', '1111',0);
-insert into Employee (login,password,myRole) values ('bob', '1111',1);
-insert into Employee (login,password,myRole) values ('charlie', '1111',1);
 
+INSERT INTO `Employee` VALUES 
+    (1,0,'alice','nLYjDiAlfv8W/u64xezI3icKNgRn9Xd0EEZFmvo9yjKyBlV3HBzJ8cJlkypahaaL'),
+    (2,1,'bob','lsGbb8VIxJnYS3DHQ5L5Z+OXcK4pH3vmbZUBeGnBnjr7UE8HH1I3a1IbcmiHwyaH'),
+    (3,1,'charlie','pZA/9HFyxQPQPuFkpPXYdJ5ZMRmn0GaDz23bbtwSZ1UEnYy1PeiP6ysPOLjQmiMB');
